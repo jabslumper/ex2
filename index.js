73c62fca93f97487aa5411ef406bbbde1423f660
jabslumper <jabslumper@gmail.com>
2019-07-13 14:13:54 +0700

    เพิ่มหน้าเทส

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('jabslumper!'))
+
 app.get('/about', (req, res) => res.send('เกี่ยวกับ!'))
 app.get('/contact', (req, res) => res.send('ติดต่อเรา!'))
+app.get('/test', (req, res) => {
+res.json({message: "test"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
